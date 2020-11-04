544775e51be37a6c587d385ae2fb41e3ccc1da69
Julian David Ramos Gomez <juli12319@hotmail.com>
2020-11-04 21:27:02 +0100

    Letra DNI

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.japo.java.main;
+
+/**
+ *
+ * @author CicloM
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        char LETRA_DNI = 'J';
+        System.out.printf("Letra DNI %c%n", LETRA_DNI);
+    }
+
+}
